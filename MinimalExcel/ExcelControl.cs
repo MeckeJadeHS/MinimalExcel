a2d85c65f72fe050a7f818414aca1d192ed04563
MeckeJadeHS <mecke@jade-hs.de>
2020-05-27 15:50:54 +0200

    init - Video version

---
+++ MinimalExcel/ExcelControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MinimalExcel
+{
+    class ExcelControl
+    {
+        ExcelControl()
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet mySheet = (Excel.Worksheet) excelApp.ActiveSheet;
+            mySheet.Cells[1, "A"] = "Hallo Welt";
+            mySheet.Cells[2, "A"] = "1";
+            mySheet.Cells[3, "A"] = "=A2+2";
+
+            // https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/interop/walkthrough-office-programming
+
+            // Console.WriteLine("Excel App erzeugt!");
+            // Console.ReadKey();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            new ExcelControl();
+        }
+    }
+}
